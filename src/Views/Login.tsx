7955dd58f9a27fd907cb701f7fb54437e7ab5b5a
Emirhan <emirhankaya356@gmail.com>
2024-06-02 13:13:08 +0300

    route düzenlemesi

--- src/Views/Login.tsx
+++ src/Views/Login.tsx
@@ -45,7 +45,7 @@ const Login: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-center">Login</h2>
+        <h2 className="text-2xl font-bold text-center">Giriş</h2>
         {error && <div className="p-4 text-sm text-red-600 bg-red-100 border border-red-400 rounded">{error}</div>}
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
@@ -60,7 +60,7 @@ const Login: React.FC = () => {
             />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Şifre</label>
             <input
               id="password"
               type="password"
@@ -75,7 +75,7 @@ const Login: React.FC = () => {
               type="submit"
               className="w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Login
+              Giriş
             </button>
           </div>
           <div>
@@ -84,7 +84,7 @@ const Login: React.FC = () => {
               type="submit"
               className="w-full px-4 py-2 text-sm font-medium text-indigo-600 border border-transparent rounded-md shadow-sm "
             >
-              Register
+              Kayıt Ol
             </NavLink>
           </div>
         </form>
--- src/Views/Register.tsx
+++ src/Views/Register.tsx
@@ -4,6 +4,7 @@ import {  useDispatch } from 'react-redux';
 import { url } from '../api';
 import { setToken } from '../authSlice';
 import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -19,18 +20,18 @@ const Register: React.FC = () => {
     e.preventDefault();
 
     if (!email || !password || !confirmPassword) {
-      setError('All fields are required');
+      setError('Boş Alan Bırakmayınız');
       return;
     }
 
     const emailRegex = /\S+@\S+\.\S+/;
     if (!emailRegex.test(email)) {
-      setError('Please enter a valid email address');
+      setError('Email Zorunludur');
       return;
     }
 
     if (password !== confirmPassword) {
-      setError('Passwords do not match');
+      setError('Şifreler Uyuşmuyor');
       return;
     }
 
@@ -49,15 +50,15 @@ const Register: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-center">Register</h2>
+        <h2 className="text-2xl font-bold text-center">Kayıt Ol</h2>
         {error && <div className="p-4 text-sm text-red-600 bg-red-100 border border-red-400 rounded">{error}</div>}
         <form onSubmit={handleSubmit} className="space-y-4">
         <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
+            <label htmlFor="name" className="block text-sm font-medium text-gray-700">İsim</label>
             <input
               id="name"
               type="name"
-              value={name}
+              value={name.toLowerCase()}
               onChange={(e) => setName(e.target.value)}
               className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               required
@@ -75,7 +76,7 @@ const Register: React.FC = () => {
             />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Şifre</label>
             <input
               id="password"
               type="password"
@@ -86,7 +87,7 @@ const Register: React.FC = () => {
             />
           </div>
           <div>
-            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
+            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Şifre Tekrar</label>
             <input
               id="confirmPassword"
               type="password"
@@ -102,8 +103,12 @@ const Register: React.FC = () => {
               type="submit"
               className="w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Register
+              Kayıt Ol
             </button>
+            <span className='flex justify-center my-1 items-center text-sm text-center text-slate-600'>
+              Zaten bir hesabınız var mı?{' '}
+              <NavLink to="/login" className="block mx-1 text-sm font-medium text-indigo-600 border border-transparent rounded-md shadow-sm ">Giriş Yap</NavLink>
+            </span>
           </div>
         </form>
       </div>
